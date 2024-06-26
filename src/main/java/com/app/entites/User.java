2f3202b9ab05a41ce5b627c379e932a81925ac1f
elint <kumarsamip03sc@gmail.com>
2024-06-26 08:01:40 +0545

    initial commit

---
+++ src/main/java/com/app/entites/User.java
@@ -0,0 +1,25 @@
+package com.app.entites;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String userId;
+
+    @NotBlank
+    private String name;
+}
